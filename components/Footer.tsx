bc5944c511b93eedeb75f42c47d791d101c2a462
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-20 11:33:27 -0500

    updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Layout from 'antd/lib/layout';
+import Container from './container';
 import CopyrightCircleOutlined from '@ant-design/icons/CopyrightCircleOutlined';
 
 const { Footer } = Layout;
@@ -7,11 +8,13 @@ const { Footer } = Layout;
 const FixedFooter: FC = () => {
 	const currentYear = new Date().getFullYear();
 	return (
-		<Footer className="align-enter text-center fff ft-3">
-			<CopyrightCircleOutlined />
-			&nbsp;
-			{currentYear}
-		</Footer>
+		<Container>
+			<Footer className="align-center text-center fff ft-3">
+				<CopyrightCircleOutlined />
+				&nbsp;
+				{currentYear}
+			</Footer>
+		</Container>
 	);
 };
 
