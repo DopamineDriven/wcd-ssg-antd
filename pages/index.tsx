a57138c359858e81a8ae9b296fbc3a6966ed2c41
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-19 22:44:44 -0500

    progress is the product

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,36 @@
-import React from "react";
-import Footer from "../components/footer";
-import Head from "next/head";
-import Header from "../components/header";
-import Affix from "antd/lib/affix";
-import Layout from "../components/layout";
-import Container from "../components/container";
+import React from 'react';
+import Footer from '../components/footer';
+import Head from 'next/head';
+import Header from '../components/header';
+import Affix from 'antd/lib/affix';
+import AppLayout from '../components/layout';
+import Container from '../components/container';
+import Layout from 'antd/lib/layout';
+
+const { Content } = Layout;
 
 interface Props {
 	props: string | number;
 }
 
 const Index = ({ props }: Props) => {
-	const user = "🐜🐜🐜SSG🐜🐜🐜BUILD🐜🐜";
+	const user = '🐜🐜🐜SSG🐜🐜🐜BUILD🐜🐜';
 	return (
 		<>
-		<Layout>
-			<Head>
-				<title>Landing Page</title>
-			</Head>
-			<Affix offsetTop={0} style={{ zIndex: 99 }}>
-				<Header props={props} />
-			</Affix>
-			<div className="pt-3">
-				<div>{user}</div>
-			</div>
-			<Footer />
-		</Layout>
+			<AppLayout>
+				<Head>
+					<title>Landing Page</title>
+				</Head>
+				<Affix offsetTop={0} style={{ zIndex: 99 }}>
+					<Header props={props} />
+				</Affix>
+				<Container>
+					<Content>
+						<div className="px-3">{user}</div>
+					</Content>
+				</Container>
+				<Footer />
+			</AppLayout>
 		</>
 	);
 };
