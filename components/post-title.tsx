f341dcbd203ef680c6bcf8c13544405f70256fbe
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-21 14:35:00 -0500

    building out posts/slug.tsx

---
+++ components/post-title.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children?: ReactNode;
+}
+
+const PostTitle = ({ children }: Props) => {
+    return (
+    <h1>{children}</h1>
+    )
+}
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,4 @@
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Container from "../../components/container";
+import FixedHeader from "../../components/header";
