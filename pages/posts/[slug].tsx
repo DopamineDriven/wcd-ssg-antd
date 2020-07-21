5184876509b4d654521de3052de83ff6108cf29f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-21 16:42:42 -0500

    built out slug.tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,15 +1,107 @@
-import { useRouter } from "next/router";
-import ErrorPage from "next/error";
-import Head from "next/head";
-import { getPostBySlug, getAllPosts } from "../../lib/api";
-import { CMS_NAME } from "../../lib/constants";
-import markdownToHtml from "../../lib/markdownToHtml";
-import PostType from "../../types/post";
-import Container from "../../components/container";
-import FixedHeader from "../../components/header";
-import AppLayout from "../../components/layout";
-
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import Head from 'next/head';
+import Link from 'next/link';
+import { getPostBySlug, getAllPosts } from '../../lib/api';
+import markdownToHtml from '../../lib/markdownToHtml';
+import PostType from '../../types/post';
+import Container from '../../components/container';
+import FixedHeader from '../../components/header';
+import AppLayout from '../../components/layout';
+import PostTitle from '../../components/post-title';
+import PostHeader from '../../components/post-header';
+import PostBody from '../../components/post-body';
+import Affix from 'antd/lib/affix';
+import Typography from 'antd/lib/typography';
 
 type Props = {
-    post: PostType;
+	props: string | number;
+	post: PostType;
+};
+
+const { Text } = Typography;
+
+const Post = ({ post, props }: Props) => {
+	const router = useRouter();
+	if (!router.isFallback && !post?.slug) {
+		return <ErrorPage statusCode={404} />;
+	}
+	return (
+		<AppLayout>
+			<Affix offsetTop={0} className='affix-header'>
+				<FixedHeader props={props} />
+			</Affix>
+			<Container>
+				<Link href='/'>
+					<Text strong className='mb-6 fff'>
+						&rarr;&nbsp;Home&nbsp;&larr;
+					</Text>
+				</Link>
+				{router.isFallback ? (
+					<PostTitle>Loading...</PostTitle>
+				) : (
+					<>
+						<article className='mb-8 fff'>
+							<Head>
+								<title>About {post.title} | WCD</title>
+								<meta property='og:image' content={post.ogImage.url} />
+							</Head>
+							<PostHeader
+								title={post.title}
+								coverImage={post.coverImage}
+								date={post.date}
+								author={post.author}
+							/>
+							<PostBody content={post.content} />
+						</article>
+					</>
+				)}
+			</Container>
+		</AppLayout>
+	);
+};
+
+export default Post;
+
+type Params = {
+	params: {
+		slug: string;
+	};
+};
+
+export async function getStaticProps({ params }: Params) {
+	const post = getPostBySlug(params.slug, [
+		'title',
+		'date',
+		'slug',
+		'author',
+		'content',
+		'ogImage',
+		'coverImage'
+	]);
+	const content = await markdownToHtml(post.content || '');
+
+	return {
+		props: {
+			post: {
+				...post,
+				content
+			}
+		}
+	};
+}
+
+export async function getStaticPaths() {
+	const posts = getAllPosts(['slug']);
+
+	return {
+		paths: posts.map(posts => {
+			return {
+				params: {
+					slug: posts.slug
+				}
+			};
+		}),
+		fallback: false
+	};
 }
